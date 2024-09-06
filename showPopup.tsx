c4a404df532c29976cb894f51a1eba791103a5b5
wlarkspur <newacon@naver.com>
2024-09-06 15:28:16 +0200

    link

--- showPopup.tsx
+++ showPopup.tsx
@@ -2,9 +2,18 @@ import WebApp from "@twa-dev/sdk";
 WebApp.ready();
 
 export default function showPopup() {
-  WebApp.showPopup({
-    title: "사람새끼 판독기",
-    message: "나는 사람 새끼인가?",
-    buttons: [{ id: "link", type: "default", text: "??" }, { type: "close" }],
-  });
+  WebApp.showPopup(
+    {
+      title: "GAY OR LESBIAN",
+      message: "당신은 LGBT를 지지하는 사람인가요?",
+      buttons: [{ id: "link", type: "default", text: "" }, { type: "close" }],
+    },
+    function (btn: any) {
+      if (btn === "link") {
+        WebApp.openLink(
+          "https://slaviccenter.osu.edu/sites/default/files/styles/news_and_events_image/public/events-images/russia-gay-pride-putin.jpg?itok=SHVKhaNr"
+        );
+      }
+    }
+  );
 }
